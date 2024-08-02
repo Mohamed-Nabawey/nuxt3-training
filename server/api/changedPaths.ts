704982a5cf7ebc1edb022e038ce0189fef999b89
Mohamed-Nabawey <135737182+Mohamed-Nabawey@users.noreply.github.com>
2024-08-02 16:48:18 +0300

    Create changedPaths.ts

---
+++ server/api/changedPaths.ts
@@ -0,0 +1,10 @@
+export default defineEventHandler(async (event) => {
+    // ... Do whatever you want here
+    console.log(event);
+    return {
+        api: [
+            '/about',
+            '/custom'
+        ],
+    }
+  })
